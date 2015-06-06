84849f12f49c04703114380c32daf64c733cf107
Ritsuo Kawashima <ritsuokbb@gmail.com>
2015-06-06 14:43:38 +0900

    大きさ調整、敵地設置。TurnEndボタン設置。

---
+++ Assets/Scripts/Battle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Battle : MonoBehaviour {
+	
+	public void isClicked(){
+		Debug.Log ("I'm Clicked");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
